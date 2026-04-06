f1b203f1396613503e5374e5794312ed2f2985a3
fdhill <muhammadfadhilahnaf11@gmail.com>
2026-04-06 07:54:54 +0700

    menambahkan class <NAMA_CLASS>

---
+++ DataMahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_7
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class DataMahasiswa
+    {
+        public Nama nama { get; set; }
+        public string nim { get; set; }
+        public string fakultas { get; set; }
+
+        public static void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("tp7_1_103082400012.json");
+            DataMahasiswa data = JsonSerializer.Deserialize<DataMahasiswa>(jsonString);
+
+            Console.WriteLine("Nama " + data.nama.depan + " " + data.nama.belakang +
+                              " dengan nim " + data.nim +
+                              " dari fakultas " + data.fakultas);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_7;
+
+namespace tpmodul7_kelompok_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa.ReadJSON();
+            Console.WriteLine();
+            DataMahasiswa.ReadJSON();
+        }
+    }
+
+}
