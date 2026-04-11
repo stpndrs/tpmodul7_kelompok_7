a7ead629f59cad25e223cc2f9f726d64add3a163
rakhabram <rakha.bramantyo@gmail.com>
2026-04-11 10:23:45 +0700

    menambahan class rakha

---
+++ Course.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+public class Course
+{
+    public string code { get; set; }
+    public string name { get; set; }
+}
+
+public class KuliahMahasiswa1301210000
+{
+    public List<Course> courses { get; set; }
+
+    public void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("tp7_2_1301210000.json");
+        KuliahMahasiswa1301210000 data = JsonSerializer.Deserialize<KuliahMahasiswa1301210000>(jsonString);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        for (int i = 0; i < data.courses.Count; i++)
+        {
+            // Output sesuai format [cite: 60, 61, 62]
+            Console.WriteLine($"MK {i + 1} {data.courses[i].code} - {data.courses[i].name}");
+        }
+    }
+}
---
+++ DataMahasiswa103082400010.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+public class DataMahasiswa103082400010
+{ 
+    public string nama { get; set; }
+    public string nim { get; set; }
+    public string fakultas { get; set; }
+
+    public void ReadJSON()
+    {
+        // Baca file json
+        string jsonString = File.ReadAllText("tp7_1_103082400010.json");
+
+        // Deserialisasi string ke Object
+        DataMahasiswa103082400010 data = JsonSerializer.Deserialize<DataMahasiswa103082400010>(jsonString);
+
+        
+        Console.WriteLine($"Nama {data.nama} dengan nim {data.nim} dari fakultas {data.fakultas}");
+    }
+}
